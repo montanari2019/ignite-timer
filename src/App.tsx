52095d172e39108fab9040deba28e9eaf3251871
montanari2019 <ikaro.montanari31@gmail.com>
2022-09-21 23:20:37 -0400

    configurando eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import { Button } from './components/Button';
-import { ThemeProvider } from "styled-components"
-import { defaultTheme } from './styles/themes/default';
+import React from 'react'
+import { Button } from './components/Button'
+import { ThemeProvider } from 'styled-components'
 
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"/>
-      <Button variant="danger"/>
-      <Button variant="secondary"/>
-      <Button variant="warning"/>
-      <Button variant="success"/>
-      <Button/>
+      <Button variant="primary" />
+      <Button variant="danger" />
+      <Button variant="secondary" />
+      <Button variant="warning" />
+      <Button variant="success" />
+      <Button />
+      <GlobalStyle />
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,36 +1,39 @@
-import styled, {css} from "styled-components";
+import styled, { css } from 'styled-components'
 
-export type ButtonVariant = 'primary' | 'danger' | 'secondary' | 'success' | 'warning'
+export type ButtonVariant =
+  | 'primary'
+  | 'danger'
+  | 'secondary'
+  | 'success'
+  | 'warning'
 
-
-interface ButtonContainerProps{
-    variant: ButtonVariant
-
+interface ButtonContainerProps {
+  variant: ButtonVariant
 }
 
 const buttonVariants = {
-    primary: 'blue',
-    secondary: 'purple',
-    success: 'green',
-    danger: 'red',
-    warning: 'yellow'
+  primary: 'blue',
+  secondary: 'purple',
+  success: 'green',
+  danger: 'red',
+  warning: 'yellow',
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
 
-    width: 100px;
-    height: 40px;
+  border-radius: 8px;
+  margin-right: 1rem;
+  border: none;
+  margin: 1rem;
 
-    border-radius: 8px;
-    margin-right: 1rem;
-    border: none;
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
 
-    background-color: ${props => props.theme.primary};
-    color: ${props => props.theme.white}
-
-  /* ${props => {
-    return css`background-color: ${buttonVariants[props.variant]}`
+  /* ${(props) => {
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `
   }} */
-
-
-`;
+`
